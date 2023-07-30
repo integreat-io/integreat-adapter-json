bc4630003a48f819a95ed3a1e2eaa80a28f4a669
Kjell-Morten <post@kjellmorten.no>
2023-07-30 21:32:32 +0200

    Implement json transformer

---
+++ src/transformer.ts
@@ -0,0 +1,24 @@
+import { isObject } from './utils/is.js'
+import type { Transformer } from 'integreat'
+
+const isParsed = (data: unknown) => Array.isArray(data) || isObject(data)
+
+function parse(data: unknown) {
+  if (typeof data === 'string') {
+    try {
+      return JSON.parse(data)
+    } catch {}
+  } else if (isParsed(data)) {
+    return data
+  }
+  return undefined
+}
+
+function stringify(data: unknown) {
+  return JSON.stringify(data)
+}
+
+const json: Transformer = () => () => (data, state) =>
+  state.rev ? stringify(data) : parse(data)
+
+export default json
