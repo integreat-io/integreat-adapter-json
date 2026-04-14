ffb375b54fc1dc8635f4f350566144e8cd65cf2c
Kjell-Morten <post@kjellmorten.no>
2026-04-14 11:51:39 +0200

    Export needed types at the top-level

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import adapter, { Logger } from './adapter/index.js'
+import adapter, { type Logger } from './adapter/index.js'
+
+export type {
+  Logger,
+  Request,
+  RequestData,
+  Response,
+  Options,
+  CompiledOptions,
+  Headers,
+  SendOptions,
+} from './adapter/index.js'
 
 export default Object.assign((logger?: Logger) => adapter(logger), {
   default: adapter,
